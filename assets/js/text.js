722820e7c1d7435d9665da30beb42122cf9f30c1
嘉何 <65057514+jiahe005@users.noreply.github.com>
2022-12-31 02:36:01 +0800

    Update text.js

--- assets/js/text.js
+++ assets/js/text.js
@@ -40,7 +40,7 @@ var binft = function (r) {
             setTimeout(i, d)
     }
     var l = "",
-        o = ["谁无暴风劲雨时，","但求与苍天","必有出头之日。",].map(function (r) {
+        o = ["你好，","我叫嘉何","你叫什么？",].map(function (r) {
             return r + "."
         }),
         a = 2,
